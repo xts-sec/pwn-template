9cf758d7a2e0d2846bb2c97c84fddd8d79263f14
xts <mnc2603@gmail.com>
2021-11-30 18:32:43 +0000

    Create pwn-template.py

---
+++ pwn-template.py
@@ -0,0 +1,73 @@
+from pwn import *
+
+# Allows you to switch between local/GDB/remote from terminal
+def start(argv=[], *a, **kw):
+    if args.GDB:  # Set GDBscript below
+        return gdb.debug([exe] + argv, gdbscript=gdbscript, *a, **kw)
+    elif args.REMOTE:  # ('server', 'port')
+        return remote(sys.argv[1], sys.argv[2], *a, **kw)
+    else:  # Run locally
+        return process([exe] + argv, *a, **kw)
+
+# Find offset to EIP/RIP for buffer overflows
+def find_ip(payload, architecture):
+    # Launch process and send payload
+    p = process(exe)
+    p.sendlineafter("ROP me outside, how 'about dah?", payload)
+    # Wait for the process to crash
+    p.wait()
+    # Print out the address of EIP/RIP at the time of crashing
+    if architecture == "x86":
+        ip_offset = cyclic_find(p.corefile.pc)  # x86
+    elif architecture == "x64":
+        ip_offset = cyclic_find(p.corefile.read(p.corefile.sp, 4))  # x64
+    else:
+        print("Invalid architecture value supplied. Breaking.")
+        exit(1);
+    info('located EIP/RIP offset at {a}'.format(a=ip_offset))
+    return ip_offset
+
+# ===========================================================
+#                    CONFIG GOES HERE
+# ===========================================================
+
+# Specify GDB script here (breakpoints etc)
+gdbscript = '''
+break main
+continue
+'''.format(**locals())
+
+# Binary filename [CHANGE THIS]
+exe = './FileName'
+
+# File architecture (x86 or x64)
+architecture = "x86"
+
+# This will automatically get context arch, bits, os etc
+elf = context.binary = ELF(exe, checksec=False)
+
+# Change logging level to help with debugging (warning/info/debug)
+context.log_level = 'debug'
+
+# ===========================================================
+#                    EXPLOIT STARTS HERE
+# ===========================================================
+
+# Pass in pattern_size, get back EIP/RIP offset
+offset = find_ip(cyclic(1000), architecture)
+
+# Start program
+io = start()
+
+#building the payload
+
+pprint(elf.got)
+
+
+payload = flat({
+    offset: [
+
+    ]
+})
+
+io.interactive()
